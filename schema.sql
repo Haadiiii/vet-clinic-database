04b721ada1278e888280608b12fc96e7768cef53
Haadiiii <hammali99@gmail.com>
2022-10-18 23:46:29 +0500

    create column for species, update and delete queries

--- schema.sql
+++ schema.sql
@@ -1,10 +1,39 @@
 /* Database schema to keep the structure of entire database. */
-
+drop TABLE if exists animals;
 CREATE TABLE animals (
-    id INT,
+    id bigserial,
     name varchar(100) NOT NULL,
+    species varchar(100) NULL,
     date_of_birth DATE NOT NULL,
     escape_attempts INT,
     neutered BOOLEAN,
-    weight_kg DECIMAL NOT NULL
+    weight_kg DECIMAL NOT NULL,
+    PRIMARY KEY (id)
 );
+
+-- WITH cte AS (
+--     SELECT 
+--         id, 
+--         name, 
+--         date_of_birth, 
+--         escape_attempts,
+--         neutered,
+--         weight_kg,
+--         ROW_NUMBER() OVER (
+--             PARTITION BY 
+--                 name, 
+--                 date_of_birth, 
+--                 escape_attempts,
+--                 neutered,
+--                 weight_kg
+--             ORDER BY 
+--             name, 
+--                 date_of_birth, 
+--                 escape_attempts,
+--                 neutered,
+--                 weight_kg
+--         ) AS row_num
+--     FROM animals
+-- )
+-- DELETE FROM cte
+-- WHERE row_num > 1;
